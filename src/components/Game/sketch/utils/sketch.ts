f41d84102dab8b60127f48f788a59964d7ab664d
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 14:51:01 -0600

    improve souls handling

--- src/components/Game/sketch/utils/sketch.ts
+++ src/components/Game/sketch/utils/sketch.ts
@@ -1,8 +1,8 @@
-import type { GameInstance, Student } from '@/types'
-import type { Soul } from '../characters/soul'
-import type { Player } from '../characters/player'
-import type { Year } from '@/types'
 import { Api } from '@/lib/api'
+import type { GameInstance, Student, Year } from '@/types'
+import { Vector } from 'p5'
+import type { Player } from '../characters/player'
+import type { Soul } from '../characters/soul'
 import { displayTooltip } from './tooltip'
 
 interface SoulState {
@@ -21,7 +21,9 @@ export async function handleSouls(p5: GameInstance, souls: Soul[], player: Playe
 	for (const soul of souls) {
 		soul.show(p5)
 
-		if (soul.collision(player.position)) {
+		const inRange = soul.isInRange(player.position)
+
+		if (inRange) {
 			let soulState = soulsCache.get(soul.getId())
 
 			if (!soulState) {
@@ -34,8 +36,8 @@ export async function handleSouls(p5: GameInstance, souls: Soul[], player: Playe
 				try {
 					const student = await Api.getStudentByIndex(year, soul.getId())
 					soulState.student = student
-					soulState.isLoading = false
 					soulsCache.set(soul.getId(), soulState)
+					soulState.isLoading = false
 				} catch (error) {
 					console.error(`Error loading student: ${error}`)
 					soulState.isLoading = false
@@ -56,11 +58,46 @@ export async function handleSouls(p5: GameInstance, souls: Soul[], player: Playe
 				tooltipVisible = true
 			}
 		}
+
+		if (soul.collision(player.position, player.size)) {
+			handleCollision(player, soul)
+		}
 	}
 
 	if (tooltipVisible) {
 		displayTooltip(p5, tooltipPosition.x, tooltipPosition.y, tooltipText)
 	}
+}
+
+export function handleCollision(player: Player, soul: Soul) {
+	const pushDirection = Vector.sub(player.position, soul.position)
+	pushDirection.normalize()
+
+	const minDistance = player.size / 2 + soul.size / 2
+	const newPos = Vector.add(soul.position, pushDirection.mult(minDistance))
+
+	player.position = newPos
+}
+
+export function drawWorld(p5: GameInstance) {
+	const gridSize = 50
+	const worldSize = 2000
+
+	p5.stroke(150)
+	p5.strokeWeight(1)
+
+	for (let x = 0; x < worldSize; x += gridSize) {
+		p5.line(x, 0, x, worldSize)
+	}
+
+	for (let y = 0; y < worldSize; y += gridSize) {
+		p5.line(0, y, worldSize, y)
+	}
+
+	p5.strokeWeight(5)
+	p5.stroke(255, 0, 0)
+	p5.noFill()
+	p5.rect(0, 0, worldSize, worldSize)
 }
 
 export function clearSoulsCache() {
