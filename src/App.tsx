a4f3f058e77960e60256fdbaed673f9c40caf6f7
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 02:23:11 -0600

    feat: lazy load components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Home } from '@/components/Home'
-import { Game } from '@/components/Game'
+import { Suspense, lazy } from 'react'
 import { useGameContext } from '@/hooks/useGameContext'
+
+const Home = lazy(() => import('@/components/Home'))
+const Game = lazy(() => import('@/components/Game'))
 
 export default function App() {
 	const { isGameStarted } = useGameContext()
 
-	return <main className='flex flex-col'>{isGameStarted ? <Game /> : <Home />}</main>
+	return (
+		<main className='flex flex-col'>
+			<Suspense fallback={<div>Loading...</div>}>{isGameStarted ? <Game /> : <Home />}</Suspense>
+		</main>
+	)
 }
