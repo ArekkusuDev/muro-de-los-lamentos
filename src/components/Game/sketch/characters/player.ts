fc3815de205c6d34f4a317cbafc03e7e86bc8cf1
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 14:50:17 -0600

    improve character methods

--- src/components/Game/sketch/characters/player.ts
+++ src/components/Game/sketch/characters/player.ts
@@ -4,6 +4,7 @@ import type { Vector } from 'p5'
 interface PlayerProps {
 	size?: number
 	velocity?: number
+	worldSize?: number
 }
 
 export class Player {
@@ -11,52 +12,51 @@ export class Player {
 	public velocity: Vector
 	public size: number
 	private character_velocity: number
+	private previousPosition: Vector
+	private worldSize: number
 
 	constructor(p5: GameInstance, props: PlayerProps) {
-		this.position = p5.createVector(p5.width / 2, p5.height / 2)
+		this.worldSize = props.worldSize ?? 2000
+		this.position = p5.createVector(this.worldSize / 2, this.worldSize / 2)
+		this.previousPosition = this.position.copy()
 		this.velocity = p5.createVector(0, 0)
 		this.size = props.size ?? 20
 		this.character_velocity = props.velocity ?? 3
 	}
 
 	public show(p5: GameInstance) {
+		p5.push()
 		p5.fill(0, 0, 255)
+		p5.strokeWeight(0)
+		p5.rectMode(p5.CENTER)
 		p5.square(this.position.x, this.position.y, this.size)
+		p5.pop()
 	}
 
 	public move(p5: GameInstance) {
+		this.previousPosition = this.position.copy()
 		const newVelocity = p5.createVector(0, 0)
 
 		// A
-		if (p5.keyIsDown(65)) {
-			newVelocity.x -= this.character_velocity
-		}
-
+		if (p5.keyIsDown(65)) newVelocity.x -= this.character_velocity
 		// D
-		if (p5.keyIsDown(68)) {
-			newVelocity.x += this.character_velocity
-		}
-
+		if (p5.keyIsDown(68)) newVelocity.x += this.character_velocity
 		// S
-		if (p5.keyIsDown(87)) {
-			newVelocity.y -= this.character_velocity
-		}
-
+		if (p5.keyIsDown(87)) newVelocity.y -= this.character_velocity
 		// W
-		if (p5.keyIsDown(83)) {
-			newVelocity.y += this.character_velocity
-		}
-
+		if (p5.keyIsDown(83)) newVelocity.y += this.character_velocity
 		// shift key to run
-		if (p5.keyIsDown(16)) {
-			newVelocity.mult(2)
-		}
+		if (p5.keyIsDown(16)) newVelocity.mult(2)
 
 		this.position.add(newVelocity)
 		this.velocity = newVelocity
 
 		// limit the player to the canvas
-		this.position.x = p5.constrain(this.position.x, 0, p5.width - this.size)
-		this.position.y = p5.constrain(this.position.y, 0, p5.height - this.size)
+		this.position.x = p5.constrain(this.position.x, this.size / 2, this.worldSize - this.size / 2)
+		this.position.y = p5.constrain(this.position.y, this.size / 2, this.worldSize - this.size / 2)
+	}
+
+	public revertPosition() {
+		this.position = this.previousPosition.copy()
 	}
 }
--- src/components/Game/sketch/characters/soul.ts
+++ src/components/Game/sketch/characters/soul.ts
@@ -1,14 +1,16 @@
 import type { GameInstance } from '@/types'
-import type { Vector } from 'p5'
+import { Vector } from 'p5'
 
 export class Soul {
 	public id: number
 	public size: number
 	public position: Vector
+	public detectionRadius: number
 
 	constructor(p5: GameInstance, id: number) {
 		this.id = id
 		this.size = 20
+		this.detectionRadius = 70
 		this.position = this.randomPosition(p5)
 	}
 
@@ -21,23 +23,32 @@ export class Soul {
 		// use fixed width and height values
 		// if we refresh the page too fast, the canvas will not be ready
 		// and we get invalid p5.width and p5.height values
-		const WIDTH = 800
-		const HEIGHT = 600
+		const WORLD_SIZE = 2000
 
-		const x = p5.random(MARGIN, WIDTH - MARGIN)
-		const y = p5.random(MARGIN, HEIGHT - MARGIN)
+		const x = p5.random(MARGIN, WORLD_SIZE - MARGIN)
+		const y = p5.random(MARGIN, WORLD_SIZE - MARGIN)
 
 		return p5.createVector(x, y)
 	}
 
 	public show(p5: GameInstance) {
+		p5.push()
+
 		p5.fill(255, 0, 0)
-		p5.circle(this.position.x, this.position.y, 20)
+		p5.circle(this.position.x, this.position.y, this.size)
+
+		p5.pop()
 	}
 
-	public collision(player_position: Vector) {
-		const distance = player_position.dist(this.position)
+	public collision(playerPosition: Vector, playerSize: number) {
+		const distance = Vector.dist(this.position, playerPosition)
 
-		return distance < this.size
+		return distance < this.size / 2 + playerSize / 2
+	}
+
+	public isInRange(playerPosition: Vector) {
+		const distance = Vector.dist(this.position, playerPosition)
+
+		return distance < this.detectionRadius
 	}
 }
