8014b04fbe80e222ffae966fc5e04c6d984ab51d
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 14:50:49 -0600

    add camera utils

---
+++ src/components/Game/sketch/utils/camera.ts
@@ -0,0 +1,11 @@
+import type { GameInstance, GameInstanceState } from '@/types'
+import type { Player } from '../characters/player'
+
+export function updateCamera(p5: GameInstance, state: GameInstanceState, player: Player) {
+	const targetX = player.position.x - p5.width / 2
+	const targetY = player.position.y - p5.height / 2
+
+	const lerp = 0.1
+	state.camera.x += (targetX - state.camera.x) * lerp
+	state.camera.y += (targetY - state.camera.y) * lerp
+}
