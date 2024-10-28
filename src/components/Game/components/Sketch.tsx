abdb4e636711f30f7abbf4e5aca5afe89e2ed48d
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 23:30:07 -0600

    feat: view props from the canvas in the parent component

--- src/components/Game/components/Sketch.tsx
+++ src/components/Game/components/Sketch.tsx
@@ -1,13 +1,18 @@
-import { ReactP5Wrapper } from '@p5-wrapper/react'
-import { gameSketch } from '../sketch/game'
-import type { Year, Student } from '@/types'
 import { Api } from '@/lib/api'
+import type { GameInfo, Student, Year } from '@/types'
+import { ReactP5Wrapper } from '@p5-wrapper/react'
 import { useEffect, useState } from 'react'
+import { gameSketch } from '../sketch/game'
 
 export function Sketch({ year }: { year: Year }) {
 	const [students, setStudents] = useState<Student[]>([])
 	const [loading, setLoading] = useState(true)
 	const [error, setError] = useState<string | null>(null)
+	const [gameInfo, setGameInfo] = useState<GameInfo>({
+		remainigStudents: 0,
+		foundStudents: 0,
+		selectedStudent: null
+	})
 
 	useEffect(() => {
 		const fetchStudents = async () => {
@@ -17,6 +22,12 @@ export function Sketch({ year }: { year: Year }) {
 			try {
 				const fetchedStudents = await Api.getStudentsByYear(year)
 				setStudents(fetchedStudents)
+				setGameInfo(prev => {
+					return {
+						...prev,
+						remainigStudents: fetchedStudents.length
+					}
+				})
 			} catch (error) {
 				console.error(`Error fetching students: ${error}`)
 				setError('Error al cargar los estudiantes desde la base de datos')
@@ -34,8 +45,8 @@ export function Sketch({ year }: { year: Year }) {
 
 	return (
 		<section className='flex gap-4'>
-			<aside className='flex flex-col py-2 px-4 bg-black text-lg font-semibold'>
-				<h2>Información del juego</h2>
+			<aside className='flex flex-col py-2 px-4 bg-[#1e222a] text-base'>
+				<h2 className='uppercase font-bold text-center'>Información del juego</h2>
 				<p>Año: {year}</p>
 				<p>Estudiantes restantes:</p>
 				<p>Estudiantes encontrados:</p>
@@ -45,13 +56,14 @@ export function Sketch({ year }: { year: Year }) {
 				sketch={gameSketch}
 				year={year}
 				students={students}
+				onUpdateGameInfo={(info: GameInfo) => setGameInfo(info)}
 			/>
 
-			<aside className='flex flex-col py-2 px-4 bg-black text-lg font-semibold'>
-				<h2>Información del estudiante</h2>
-				<p>Nombre:</p>
-				<p>Semestre:</p>
-				<p>Grupo:</p>
+			<aside className='flex flex-col py-2 px-4 bg-[#1e222a] text-base'>
+				<h2 className='uppercase font-bold text-center'>Información del estudiante</h2>
+				<p>Nombre: {gameInfo.selectedStudent?.name || '-'}</p>
+				<p>Apellidos: {gameInfo.selectedStudent?.lastnames || '-'}</p>
+				<p>Apodo: {gameInfo.selectedStudent?.nickname || '-'}</p>
 			</aside>
 		</section>
 	)
