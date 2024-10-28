ed6401cd57d271ffda0b03cba3d27d96a241e80f
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-27 19:53:54 -0600

    update soul image

--- src/components/Game/sketch/characters/soul.ts
+++ src/components/Game/sketch/characters/soul.ts
@@ -1,42 +1,47 @@
 import type { GameInstance } from '@/types'
-import { Vector } from 'p5'
+import type { GameMap } from '../map/GameMap'
+import { Vector, Image } from 'p5'
+
+interface SoulProps {
+	id: number
+	gameMap: GameMap
+	image: Image | null
+}
 
 export class Soul {
 	public id: number
 	public size: number
 	public position: Vector
 	public detectionRadius: number
+	private image: Image | null
 
-	constructor(p5: GameInstance, id: number) {
-		this.id = id
-		this.size = 20
+	constructor(p5: GameInstance, props: SoulProps) {
+		this.id = props.id
+		this.size = 40
+		this.image = props.image
 		this.detectionRadius = 70
-		this.position = this.randomPosition(p5)
+		this.position = props.gameMap.getRandomPosition(p5, 1)
 	}
 
 	public getId() {
 		return this.id
 	}
 
-	private randomPosition(p5: GameInstance) {
-		const MARGIN = 30
-		// use fixed width and height values
-		// if we refresh the page too fast, the canvas will not be ready
-		// and we get invalid p5.width and p5.height values
-		const WORLD_SIZE = 2000
-
-		const x = p5.random(MARGIN, WORLD_SIZE - MARGIN)
-		const y = p5.random(MARGIN, WORLD_SIZE - MARGIN)
-
-		return p5.createVector(x, y)
+	public draw(p5: GameInstance) {
+		if (this.image) {
+			p5.push()
+			p5.imageMode(p5.CENTER)
+			p5.image(this.image, this.position.x, this.position.y, this.size, this.size)
+			p5.pop()
+		} else {
+			this.drawFallback(p5)
+		}
 	}
 
-	public show(p5: GameInstance) {
+	private drawFallback(p5: GameInstance) {
 		p5.push()
-
 		p5.fill(255, 0, 0)
 		p5.circle(this.position.x, this.position.y, this.size)
-
 		p5.pop()
 	}
 
