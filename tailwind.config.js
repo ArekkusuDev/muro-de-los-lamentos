4f244c7933484176faf76fb97fb68f7e2edd5126
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-29 02:31:05 -0600

    add text-shadow class utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px rgba(0, 0, 0, 0.8)',
+				DEFAULT: '0 2px 4px rgba(0, 0, 0, 0.8)',
+				lg: '0 8px 16px rgba(0, 0, 0, 0.8)'
+			}
+		}
 	},
-	plugins: []
+	plugins: [
+		function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': value => ({
+						textShadow: value
+					})
+				},
+				{ values: theme('textShadow') }
+			)
+		}
+	]
 }
