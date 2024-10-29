7d41f602e539f40c9e6e3df8ef1beb9509a06c96
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-29 02:30:12 -0600

    make text of the button more readable

--- src/components/Game/components/CardButton.tsx
+++ src/components/Game/components/CardButton.tsx
@@ -4,11 +4,11 @@ export function CardButton({ text, ...props }: ButtonProps) {
 	return (
 		<button
 			{...props}
-			className='card-button relative overflow-hidden text-white border-2 border-white rounded-lg px-4 py-2 text-lg transition duration-300 ease-in-out
+			className='card-button relative overflow-hidden text-white border-2 border-white rounded-lg px-6 py-3 text-2xl transition duration-300 ease-in-out
       filter grayscale hover:grayscale-0'
 		>
-			<div className='absolute inset-0 bg-black opacity-50 hover:opacity-0 transition duration-300 ease-in-out'></div>
-			<span className='relative z-10 hover:text-orange-600 transition duration-300 ease-in-out'>
+			<div className='absolute inset-0 bg-black opacity-70 hover:opacity-30 transition duration-300 ease-in-out'></div>
+			<span className='relative z-10 font-bold text-shadow-sm'>
 				{text}
 			</span>
 		</button>
