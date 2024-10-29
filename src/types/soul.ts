da7ba94ac73d0771f1c5f7afd6538c5ed7787cfd
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-29 14:54:17 -0600

    move types to a directory

---
+++ src/types/soul.ts
@@ -0,0 +1,17 @@
+import type { Soul } from '@/components/Game/sketch/characters/soul'
+import type { GameMap } from '@/components/Game/sketch/map/GameMap'
+import type { Student } from '@/types'
+import type { Image } from 'p5'
+
+export type SoulProps = {
+	id: string
+	map: GameMap
+	image: Image | null
+}
+
+export type SoulState = {
+	soul: Soul
+	student?: Student
+	isLoading: boolean
+	found?: boolean
+}
