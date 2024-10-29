8e09a49c51763fcdff65c8e9fc1ab975c6270ff3
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-29 08:45:42 -0600

    update Sketch component

--- src/components/Game/components/Sketch.tsx
+++ src/components/Game/components/Sketch.tsx
@@ -3,10 +3,12 @@ import type { GameInfo, Student, Year } from '@/types'
 import { ReactP5Wrapper } from '@p5-wrapper/react'
 import { useEffect, useState } from 'react'
 import { gameSketch } from '../sketch/game'
+import { GameLayoutWrapper } from './GameLayoutWrapper'
 
 export function Sketch({ year }: { year: Year }) {
 	const [students, setStudents] = useState<Student[]>([])
 	const [loading, setLoading] = useState(true)
+	const [p5Loading, setP5Loading] = useState(true)
 	const [error, setError] = useState<string | null>(null)
 	const [gameInfo, setGameInfo] = useState<GameInfo>({
 		remainigStudents: 0,
@@ -38,38 +40,39 @@ export function Sketch({ year }: { year: Year }) {
 		fetchStudents()
 	}, [year])
 
-	if (loading) return <div>Loading students...</div>
+	const handleSketchSetup = () => {
+		setP5Loading(false)
+	}
 
-	if (error) return <div>{error}</div>
+	const LoadingPlaceholder = () => (
+		<div className='flex items-center justify-center w-[3000px] h-[600px] bg-gray-800 rounded-lg'>
+			<div className='animate-spin rounded-full h-16 w-16 border-b-2 border-purple-500'></div>
+		</div>
+	)
+
+	if (error)
+		return (
+			<GameLayoutWrapper>
+				<div className='flex items-center justify-center w-full h-[600px] bg-gray-800 rounded-lg'>
+					<p className='text-red-500 text-xl'>{error}</p>
+				</div>
+			</GameLayoutWrapper>
+		)
 
 	return (
-		<section className='flex gap-4 w-11/12 justify-center text-wrap'>
-			<aside className='flex w-full flex-col py-2 px-4 bg-gray-800 rounded-lg shadow-lg min-w-[200px]'>
-				<h2 className='text-xl mb-4 text-purple-400 uppercase font-bold text-center'>
-					Información del juego
-				</h2>
-				<div className='space-y-2'>
-					<p className='flex justify-between'>
-						<span className='font-bold'>Año:</span>
-						<span className='font-semibold'>{year}</span>
-					</p>
-					<p className='flex justify-between'>
-						<span className='font-bold'>Almas restantes:</span>
-						<span className='font-semibold text-orange-400'>{gameInfo.remainigStudents}</span>
-					</p>
-					<p className='flex justify-between'>
-						<span className='font-bold'>Almas encontradas:</span>
-						<span className='font-semibold text-green-400'>{gameInfo.foundStudents}</span>
-					</p>
-				</div>
-			</aside>
-
+		<GameLayoutWrapper
+			year={year}
+			remainigStudents={gameInfo.remainigStudents}
+			foundStudents={gameInfo.foundStudents}
+		>
+			{(p5Loading || loading) && <LoadingPlaceholder />}
 			<ReactP5Wrapper
 				sketch={gameSketch}
 				year={year}
 				students={students}
 				onUpdateGameInfo={(info: GameInfo) => setGameInfo(info)}
+				onSetup={handleSketchSetup}
 			/>
-		</section>
+		</GameLayoutWrapper>
 	)
 }
