34a882cb5f7f7eee49f5275c36dad0cb637e6e75
ArekkusuDev <arekkusu.dev@outlook.com>
2024-10-31 19:28:37 -0600

    change components font

--- src/components/Game/components/CardButton.tsx
+++ src/components/Game/components/CardButton.tsx
@@ -4,7 +4,7 @@ export function CardButton({ text, ...props }: ButtonProps) {
 	return (
 		<button
 			{...props}
-			className='card-button relative overflow-hidden text-white border-2 border-white rounded-lg px-6 py-3 text-2xl transition duration-300 ease-in-out
+			className='font-creepster tracking-widest card-button relative overflow-hidden text-white border-2 border-white rounded-lg px-6 py-3 text-2xl transition duration-300 ease-in-out
       filter grayscale hover:grayscale-0'
 		>
 			<div className='absolute inset-0 bg-black opacity-70 hover:opacity-30 transition duration-300 ease-in-out'></div>
--- src/components/Game/components/GameLayoutWrapper.tsx
+++ src/components/Game/components/GameLayoutWrapper.tsx
@@ -14,25 +14,23 @@ export function GameLayoutWrapper({
 	foundStudents
 }: LayoutWrappperProps) {
 	return (
-		<section className='flex gap-4 w-11/12 justify-center text-wrap'>
-			<aside className='flex w-full flex-col py-2 px-4 bg-gray-800 rounded-lg shadow-lg min-w-[200px]'>
-				<h2 className='text-xl mb-4 text-purple-400 uppercase font-bold text-center'>
+		<section className='font-jolly-lodger gap-4 text-2xl tracking-wider flex w-11/12 justify-center text-wrap'>
+			<aside className='flex flex-col gap-4 w-full p-5 bg-gray-800 rounded-lg shadow-lg min-w-[200px]'>
+				<h2 className='font-creepster text-3xl mb-4 text-purple-400 uppercase font-bold text-center'>
 					Información del juego
 				</h2>
 				<div className='space-y-2'>
 					<p className='flex justify-between'>
 						<span className='font-bold'>Año:</span>
-						{year && <span className='font-semibold'>{year}</span>}
+						<span className='font-semibold'>{year}</span>
 					</p>
 					<p className='flex justify-between'>
 						<span className='font-bold'>Almas restantes:</span>
-						{remainigStudents && (
-							<span className='font-semibold text-orange-400'>{remainigStudents}</span>
-						)}
+						<span className='font-semibold text-orange-400'>{remainigStudents}</span>
 					</p>
 					<p className='flex justify-between'>
 						<span className='font-bold'>Almas encontradas:</span>
-						{foundStudents && <span className='font-semibold text-green-400'>{foundStudents}</span>}
+						<span className='font-semibold text-green-400'>{foundStudents}</span>
 					</p>
 				</div>
 			</aside>
