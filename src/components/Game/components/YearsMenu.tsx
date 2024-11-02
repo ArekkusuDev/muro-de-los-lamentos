a2b6fd337163844b0165151da473580eb73685c4
ArekkusuDev <arekkusu.dev@outlook.com>
2024-11-02 01:51:03 -0600

    move years menu stuff in his own component

--- src/components/Game/components/YearsMenu.tsx
+++ src/components/Game/components/YearsMenu.tsx
@@ -1,25 +1,39 @@
 import type { Year } from '@/types/api'
 import { CardButton } from './CardButton'
 import { memo } from 'react'
+import { ExitButton } from './ExitButton'
+import { useGameContext } from '@/hooks/useGameContext'
 
-interface YearsMenuProps {
-	yearsList: Year[]
-	setYear: (year: Year | undefined) => void
-}
+export const YearsMenu = memo(function YearsMenu({ yearsList }: { yearsList: Year[] }) {
+	const { year, toggleGameStart, setYear } = useGameContext()
 
-export const YearsMenu = memo(function YearsMenu({ yearsList, setYear }: YearsMenuProps) {
 	return (
-		<div className='flex flex-col gap-5 p-6 lg:p-8'>
-			<h2 className='font-jolly-lodger text-3xl tracking-wider'>Selecciona un año</h2>
-			<div className='flex flex-col gap-4'>
-				{yearsList.map(year => (
-					<CardButton
-						key={year}
-						onClick={() => setYear(year)}
-						text={year}
-					/>
-				))}
+		<section className='flex flex-col min-h-[calc(100vh-4.5rem)] items-center gap-2 py-2'>
+			<div className='flex flex-col w-11/12 gap-4 justify-between items-center py-2 md:py-0 lg:flex-row'>
+				<h1 className='font-title text-center text-4xl/none py-8 md:text-5xl uppercase'>
+					El Muro de{' '}
+					<span className='text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-pink-500'>
+						Los Lamentos
+					</span>
+				</h1>
+				<ExitButton
+					year={year}
+					setYear={setYear}
+					toggleGameStart={toggleGameStart}
+				/>
 			</div>
-		</div>
+			<div className='flex flex-col gap-5 p-6 lg:p-8'>
+				<h2 className='font-jolly-lodger text-3xl tracking-wider'>Selecciona un año</h2>
+				<div className='flex flex-col gap-4'>
+					{yearsList.map(year => (
+						<CardButton
+							key={year}
+							onClick={() => setYear(year)}
+							text={year}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
 	)
 })
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -3,12 +3,11 @@ import { Api } from '@/lib/api'
 import type { Year } from '@/types/api'
 import { useEffect, useState } from 'react'
 import { LoadingSpinner } from '../LoadingSpinner'
-import { ExitButton } from './components/ExitButton'
 import { Sketch } from './components/Sketch'
 import { YearsMenu } from './components/YearsMenu'
 
 export default function Game() {
-	const { year, setYear, toggleGameStart } = useGameContext()
+	const { year } = useGameContext()
 	const [yearsList, setYearList] = useState<Year[]>([])
 	const [loading, setLoading] = useState(true)
 
@@ -29,30 +28,5 @@ export default function Game() {
 
 	if (loading) return <LoadingSpinner />
 
-	return (
-		<section className='flex flex-col h-full items-center gap-2 py-2'>
-			<header className='flex flex-col w-11/12 gap-8 justify-between items-center py-2 lg:flex-row'>
-				<h1 className='font-title text-center mb-4 text-4xl lg:text-5xl uppercase'>
-					El Muro de{' '}
-					<span className='text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-pink-500'>
-						Los Lamentos
-					</span>
-				</h1>
-				<ExitButton
-					year={year}
-					setYear={setYear}
-					toggleGameStart={toggleGameStart}
-				/>
-			</header>
-
-			{year ? (
-				<Sketch year={year} />
-			) : (
-				<YearsMenu
-					yearsList={yearsList}
-					setYear={setYear}
-				/>
-			)}
-		</section>
-	)
+	return <>{year ? <Sketch year={year} /> : <YearsMenu yearsList={yearsList} />}</>
 }
