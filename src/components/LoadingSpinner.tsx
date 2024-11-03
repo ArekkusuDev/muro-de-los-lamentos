f747cfdc975155075b0fe8e073a01f637d73bb87
ArekkusuDev <arekkusu.dev@outlook.com>
2024-11-02 23:13:22 -0600

    update loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,11 @@
 export function LoadingSpinner() {
 	return (
-		<div className='flex items-center justify-center h-[calc(100vh-4.5rem)]'>
-			<div className='animate-spin rounded-full h-32 w-32 border-b-2 border-purple-500'></div>
+		<div className='flex items-center justify-center h-[calc(100vh-4.5rem)] w-full'>
+			<div
+				className='border-[#CBA6F7] h-20 w-20 animate-spin rounded-full border-8 border-t-transparent'
+				role='status'
+				aria-label='loading'
+			></div>
 		</div>
 	)
 }
