8e3845d3fc6bd0f9cc4a91db377ae57979886043
ArekkusuDev <arekkusu.dev@outlook.com>
2024-11-04 19:32:14 -0600

    add if the student is alive or not, also fix displaying wrong value in group section

--- src/components/Game/components/StudentCard.tsx
+++ src/components/Game/components/StudentCard.tsx
@@ -1,17 +1,21 @@
 import type { Student } from '@/types/api'
 
 export function StudentCard({ student }: { student: Student }) {
+	const isAlive = student.code.length === 3
+
 	return (
 		<div className='relative flex items-center max-w-sm bg-base rounded-lg overflow-hidden my-4 text-xl min-w-80 tracking-wider'>
 			<div className='p-4'>
 				<h1 className='text-3xl mb-2'>{student.name}</h1>
 				<p>Apellidos: {student.lastnames}</p>
 				<p>Matrícula: {student.student_id}</p>
-				<p>Grupo: {student.student_id}</p>
+				<p>Grupo: {student.group.toUpperCase()}</p>
 				<p>Email: {student.email}</p>
 			</div>
-			<span className='absolute top-0 right-0 text-md me-2 px-3 py-1 rounded-b-md bg-yellow-900 text-yellow-300'>
-				Muerto
+			<span
+				className={`absolute text-center min-w-[71px] top-0 right-0 text-md me-2 px-3 py-1 rounded-b-md ${isAlive ? 'bg-green-900 text-green-300' : 'bg-gray-700 text-gray-300'}`}
+			>
+				{isAlive ? 'Vivo' : 'Muerto'}
 			</span>
 		</div>
 	)
